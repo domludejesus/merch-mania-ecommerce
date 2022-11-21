d5e15a104765cfc7ab4bb9dddeb68b505f283c0b
Dominic DeJesus <domludejesus@gmail.com>
2022-11-20 20:28:48 -0500

    creating admin products page

--- merch-mania-admin/src/App.js
+++ merch-mania-admin/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import UserList from "./pages/userList/UserList"
 import User from "./pages/user/User"
 import NewUser from "./pages/newPage/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -16,8 +17,11 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/users" element={<UserList />} />
-            <Route path="/user/:userID" element={<User />} />
+            <Route path="/user/:userId" element={<User />} />
             <Route path="/newUser" element={<NewUser />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/products/:productsId" element={<User />} />
+            <Route path="/newproduct" element={<NewUser />} />
           </Routes>
       </div>
     </Router>
