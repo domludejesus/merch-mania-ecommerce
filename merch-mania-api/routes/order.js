64219536f622e9acf0982b89af4c8403e67c2186
Dominic DeJesus <domludejesus@gmail.com>
2022-11-27 13:35:40 -0500

    Added cart and Orders income functions and routes

--- merch-mania-api/routes/order.js
+++ merch-mania-api/routes/order.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 
 // create 
 router.post("/", verifyToken, async (req, res) => { // any user can create order
-    const newOrder = new Product(req.body);
+    const newOrder = new Order(req.body);
 
     try {
         const savedOrder = await newOrder.save();
@@ -70,6 +70,27 @@ router.get("/income", verifyTokenAndAdmin, async (req,res) => {  // we only want
     const date = new Date(); 
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1)); 
     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1))
+
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { $gte: previousMonth } } }, 
+            {
+              $project: {
+                month: { $month: "$createdAt" }, 
+                sales: "$amount"
+              },
+            },
+            {
+                $group: {
+                    _id: "$month", 
+                    total: { $sum: "$sales"},
+                },
+            }, 
+        ]); 
+        res.status(200).json(income)
+    } catch (err) {
+        res.status(500).json(err); 
+    }
 })
 
 
