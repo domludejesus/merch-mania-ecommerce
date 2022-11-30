5c1bf4f40405f2c87390dc47963ab743cab159b2
Dominic DeJesus <domludejesus@gmail.com>
2022-11-30 18:00:29 -0500

    fetch and filter products done

--- merch-mania/src/components/Products.jsx
+++ merch-mania/src/components/Products.jsx
@@ -11,30 +11,49 @@ const Container = styled.div`
     justify-content: space-between;   
 `
 
-const Products = ({cat,filters,sort}) => {
-    
-    const [products,setProducts] = useState([]);
-    const [filteredProducts, setFilteredProducts] = useState([]);
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-
-
-
-useEffect(() => {
+  useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/products?category=jacket");
-        console.log(res.data)
-      } catch (err) {}
+        const res = await axios.get(
+          cat ? `http://localhost:5001/api/products?category=${cat}` : "http://localhost:5001/api/products"); // search based on query category if not use all products 
+        setProducts(res.data);
+      } catch (err) { }
     };
     getProducts();
   }, [cat]);
 
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) => Object.entries(filters).every(([key, value]) => item[key].includes(value))) // filtering products through search query 
+      );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if ((sort === "newest")) {
+      setFilteredProducts((prev) => [...prev].sort((b,c) => b.createdAt - c.createdAt) // displaying newest items for sort 
+      );
+    } else if ((sort === "asc")) {
+      setFilteredProducts((prev) => [...prev].sort((b, c) => b.price - c.price) // displaying prices sorting higher
+      );
+    } else {
+      setFilteredProducts((prev) => [...prev].sort((b, c) => c.price - b.price) // displaying lower price 
+      );
+    }
+  }, [sort]); 
+
 
     return (
         <Container>
-            {popularProducts.map((item) => (
+            {cat ? filteredProducts.map((item) => (
                 <Product item={item} key={item.id} />
-            ))}
+            )) : products.slice(0,8).map((item) => (
+              <Product item={item} key={item.id} />
+            )) }
 
         </Container>
     )
