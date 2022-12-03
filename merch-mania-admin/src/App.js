309795b5cc7acd3fb6f178aacc556e55ba3b57c6
Dominic DeJesus <domludejesus@gmail.com>
2022-12-03 16:47:25 -0500

    updating admin.js

--- merch-mania-admin/src/App.js
+++ merch-mania-admin/src/App.js
@@ -2,7 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css"
 import Home from "./pages/home/Home"; 
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import UserList from "./pages/userList/UserList"
 import User from "./pages/user/User"
 import NewUser from "./pages/newPage/NewUser";
@@ -12,22 +12,30 @@ import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 
 function App() {
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin; 
   return (
     <Router>
-          <Topbar/>
-            <div className="container">
-                <Sidebar />
-                <Routes>
-                  <Route path="/" element={<Home/>} />
-                  <Route path="/users" element={<UserList />} />
-                  <Route path="/user/:userId" element={<User />} />
-                  <Route path="/newUser" element={<NewUser />} />
-                  <Route path="/products" element={<ProductList />} />
-                  <Route path="/product/:productsId" element={<Product />} />
-                  <Route path="/newproduct" element={<NewProduct />} />
-                <Route path="/login" element={<Login />} />
-                </Routes>      
-            </div>   
+      <Routes>
+        <Route path="/login" element={User ? <Navigate to="/" /> : <Login />} />
+      </Routes>
+          {admin && (
+            <>
+              <Topbar/>
+              <div className="container">
+                  <Sidebar />
+                  <Routes>
+                    <Route exact path="/" element={<Home/>} />
+                    <Route path="/users" element={<UserList />} />
+                    <Route path="/user/:userId" element={<User />} />
+                    <Route path="/newUser" element={<NewUser />} />
+                    <Route path="/products" element={<ProductList />} />
+                    <Route path="/product/:productsId" element={<Product />} />
+                    <Route path="/newproduct" element={<NewProduct />} />
+                  
+                  </Routes>      
+              </div> 
+           </>
+          )}   
     </Router>
   );
 }
