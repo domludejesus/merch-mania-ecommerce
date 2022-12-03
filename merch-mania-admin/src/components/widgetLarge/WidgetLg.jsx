7106e27875665437b822496082b6cc2988cc5d2a
Dominic DeJesus <domludejesus@gmail.com>
2022-12-03 17:52:11 -0500

    bug fix widgets

--- merch-mania-admin/src/components/widgetLarge/WidgetLg.jsx
+++ merch-mania-admin/src/components/widgetLarge/WidgetLg.jsx
@@ -24,29 +24,32 @@ export default function WidgetLg() {
     <div className="widgetLg">
         <h3 className="widgetLgTitle"> Latest Transactions</h3>
         <table className="widgetLgTable">
-            <tr className="widgetLgTr">
-                <th className="widgetLgTh"> Customer</th>
-                <th className="widgetLgTh"> Date</th>
-                <th className="widgetLgTh"> Amount</th>
-                <th className="widgetLgTh"> Status</th>
-            </tr>
+            <thead>
+                <tr className="widgetLgTr">
+                    <th className="widgetLgTh"> Customer</th>
+                    <th className="widgetLgTh"> Date</th>
+                    <th className="widgetLgTh"> Amount</th>
+                    <th className="widgetLgTh"> Status</th>
+                </tr>
+            </thead>
             {orders.map(order=>(
 
-            
-            <tr className="widgetLgTr">
-                <td className="widgetLgUser">
-                    <img 
-                        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80"
-                        alt="" 
-                        className="widgetLgImg" 
-                    />
-                    <span className="widgetLgName"> {order.userId} </span>
-                </td>
-                <td className="widgetLgDate"> {format(order.createdAt)}</td>
-                <td className="widgetLgAmount">$ {order.amount} </td>
-                <td className="widgetLgStatus"> <Button type={order.status} /></td>
+            <tbody>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img 
+                            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80"
+                            alt="" 
+                            className="widgetLgImg" 
+                        />
+                        <span className="widgetLgName"> {order.userId} </span>
+                    </td>
+                    <td className="widgetLgDate"> {format(order.createdAt)}</td>
+                    <td className="widgetLgAmount">$ {order.amount} </td>
+                    <td className="widgetLgStatus"> <Button type={order.status} /></td>
 
-            </tr>
+                </tr>
+            </tbody>
         ))}
         </table>
     </div>
