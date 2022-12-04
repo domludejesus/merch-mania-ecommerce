7cd304eda78707150db01eacd9263ca78a938055
Dominic DeJesus <domludejesus@gmail.com>
2022-12-04 17:32:50 -0500

    file firebase storage upload

---
+++ merch-mania-admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC2hyGsq81_frInK2NfjHw0zfgh7jCDwLY",
+    authDomain: "merch-mania-72a88.firebaseapp.com",
+    projectId: "merch-mania-72a88",
+    storageBucket: "merch-mania-72a88.appspot.com",
+    messagingSenderId: "1017538390807",
+    appId: "1:1017538390807:web:5167d5ac49a4a08fe66552"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
--- merch-mania-admin/src/pages/newProduct/NewProduct.jsx
+++ merch-mania-admin/src/pages/newProduct/NewProduct.jsx
@@ -1,10 +1,15 @@
 import "./newProduct.css"
 import { useState } from "react"
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from "../../firebase";
+import { addProduct } from "../../redux/apiCalls"
+import { useDispatch } from "react-redux"
 
 export default function NewProduct() {
     const [ inputs, setInputs] = useState({})
     const [file, setFile] = useState(null)
     const [ cat, setCat] = useState([])
+    const dispatch = useDispatch(); 
 
     const handleChange = (e) => {
         setInputs(prev=>{
@@ -17,15 +22,53 @@ export default function NewProduct() {
 
     const handleClick = (e) => {
         e.preventDefault(); 
-        //to do 
+        const fileName = new Date().getTime() + file.name; 
+        const storage = getStorage(app)
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        uploadTask.on('state_changed',
+        (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+                default:
+            }
+        },
+        (error) => {
+            // Handle unsuccessful uploads
+        },
+        () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                const product = {...inputs, img:downloadURL, categories: cat}; // use consolelog for test instead of const 
+                addProduct(product, dispatch); 
+            });
+        }
+    );
     }
+
+    //console.log(file)
   return (
     <div className="newProduct">
         <h1 className="addProductTitle"> New Product </h1>
         <form className="addProductForm">
             <div className="addProductItem">
                 <label> Image </label>
-                <input type="file" id="file" onChange={e=>setFile(e.target.files)[0]} />
+                <input type="file" id="file" onChange={e=>setFile(e.target.files[0])} />
             </div>
             <div className="addProductItem">
                 <label> Title </label>
